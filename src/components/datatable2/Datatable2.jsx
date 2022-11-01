ede747b8ed0f7798d9d9ea169d2eecfb498570d4
OlaniyiHope <olaniyihoppee@gmail.com>
2022-11-01 17:50:39 +0100

    changed api

--- src/components/datatable2/Datatable2.jsx
+++ src/components/datatable2/Datatable2.jsx
@@ -18,7 +18,9 @@ const Datatable2 = ({ columns }) => {
 
   const handleDelete = async (id) => {
     try {
-      await axios.delete(`https://hybridhome-api.herokuapp.com/${path}/${id}`);
+      await axios.delete(
+        `https://hybridhome-api.herokuapp.com/api/hotels/${id}`
+      );
       setList(list.filter((item) => item._id !== id));
       alert("successfully deleted");
     } catch (err) {}
