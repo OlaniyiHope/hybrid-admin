d1c71c37b2612e6be8b19535656f03b97bc1837b
OlaniyiHope <olaniyihoppee@gmail.com>
2022-11-07 04:55:06 +0100

    all

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ function App() {
                 }
               />
             </Route>
-            <Route path="property">
+            <Route path="hotels">
               <Route
                 index
                 element={
